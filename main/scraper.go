260ca8d631cd7e001fda994f4160bf51c32019be
hapticfish <jquinmail@gmail.com>
2023-11-17 01:18:54 -0300

    Added additional go routines for additonal concurrent functionalith with CoinDesk Body Scraping. and 80 Char max with for txt. results.

--- main/scraper.go
+++ main/scraper.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 	"sync"
 
-	"github.com/PuerkitoBio/goquery" // GoQuery package for parsing HTML
+	"github.com/PuerkitoBio/goquery"
 )
 
 func Scrape(url string, writer io.Writer, wg *sync.WaitGroup, mutex *sync.Mutex) {
+	fmt.Printf("Starting scraping: %s...\n", url)
 	defer wg.Done()
 
 	// Send HTTP GET request
@@ -28,28 +30,11 @@ func Scrape(url string, writer io.Writer, wg *sync.WaitGroup, mutex *sync.Mutex)
 		return
 	}
 
-	// Determine the selector based on the URL
-	var selector string
 	if strings.Contains(url, "cointelegraph.com") {
-		selector = ".main-news-controls__wrap > a"
-	} else if strings.Contains(url, "coindesk.com") {
-		selector = "a.card-imagestyles__CardImageWrapper-sc-1kbd3qh-0.WDSwd"
-	}
+		doc.Find(".main-news-controls__wrap > a").Each(func(i int, s *goquery.Selection) {
+			text := wrapText(s.Text(), 80)
+			textToWrite := fmt.Sprintf("Cointelegraph: %s\n\n", text)
 
-	// Process the HTML
-	doc.Find(selector).Each(func(i int, s *goquery.Selection) {
-		var textToWrite string
-		if selector == ".main-news-controls__wrap > a" {
-			text := s.Text()
-			textToWrite = fmt.Sprintf("Cointelegraph: %s\n", text)
-		} else {
-			href, exists := s.Attr("href")
-			if exists {
-				textToWrite = fmt.Sprintf("CoinDesk: URL: %s\n", href)
-			}
-		}
-
-		if textToWrite != "" {
 			mutex.Lock()
 			_, err := writer.Write([]byte(textToWrite))
 			mutex.Unlock()
@@ -57,7 +42,82 @@ func Scrape(url string, writer io.Writer, wg *sync.WaitGroup, mutex *sync.Mutex)
 			if err != nil {
 				fmt.Println(err)
 			}
-		}
+		})
+	} else if strings.Contains(url, "coindesk.com") {
+		semaphore := make(chan struct{}, 10) // Semaphore to limit to 10 goroutines
+		var innerWg sync.WaitGroup
 
+		doc.Find("a.card-imagestyles__CardImageWrapper-sc-1kbd3qh-0.WDSwd").Each(func(i int, s *goquery.Selection) {
+			href, exists := s.Attr("href")
+			if exists {
+				semaphore <- struct{}{} // Acquire a token
+				innerWg.Add(1)
+				go func(url string) {
+					defer innerWg.Done()
+					scrapeCoinDeskArticle("https://www.coindesk.com"+url, writer, mutex)
+					<-semaphore // Release the token
+				}(href)
+			}
+		})
+
+		innerWg.Wait()   // Wait for all scraping goroutines to finish
+		close(semaphore) // Close the semaphore channel
+	}
+
+	fmt.Printf("Finished scraping: %s...\n", url)
+}
+
+func scrapeCoinDeskArticle(articleURL string, writer io.Writer, mutex *sync.Mutex) {
+	resp, err := http.Get(articleURL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var articleBody string
+	doc.Find("div.contentstyle__StyledWrapper-sc-g5cdrh-0.gkcZwU.composer-content").Each(func(i int, s *goquery.Selection) {
+		articleBody += wrapText(s.Text(), 80) + "\n\n"
 	})
+
+	mutex.Lock()
+	defer mutex.Unlock()
+	_, err = writer.Write([]byte("CoinDesk: URL: " + articleURL + "\n" + indentString(articleBody, "    ") + "\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func wrapText(text string, lineWidth int) string {
+	var wrappedText bytes.Buffer
+	currentLineLength := 0
+
+	for _, word := range strings.Fields(text) {
+		if currentLineLength+len(word)+1 > lineWidth {
+			wrappedText.WriteString("\n")
+			currentLineLength = 0
+		}
+		if currentLineLength > 0 {
+			wrappedText.WriteString(" ")
+			currentLineLength++
+		}
+		wrappedText.WriteString(word)
+		currentLineLength += len(word)
+	}
+
+	return wrappedText.String()
+}
+
+func indentString(str, indent string) string {
+	var indentedText string
+	for _, line := range strings.Split(str, "\n") {
+		indentedText += indent + line + "\n"
+	}
+	return indentedText
 }
